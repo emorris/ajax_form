8b1c7c8231cec4155192b74e3b8786415b13b1cc
johnb <johnb@Js-MacBook-Air.local>
2016-03-06 21:26:54 -0500

    my version

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,7 +1,10 @@
 $(document).on('ready page:load',function(){
    $("#new_user").on("ajax:success", function(e, data, status, xhr){
-    console.log(xhr.getAllResponseHeaders())
-    console.log("devise hitting back good")
+	   console.log(xhr.getAllResponseHeaders());
+	   
+	   $('#user_area').html('<li><a rel="nofollow" data-method="delete" href="/users/sign_out">Logout</a></li>');
+	  
+	   console.log("devise hitting back good");
    }).on("ajax:error", function (e, xhr, status, error){
      console.log("devise hitting back bad")
    })
